35806af2c94a1d8d8160d0bb8bb93d2193293ae6
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-16 19:14:23 -0400

    Set state after module initialization

--- dbw/node_bl/src/sys/entry.c
+++ dbw/node_bl/src/sys/entry.c
@@ -8,10 +8,10 @@ const enum firmware_module_types FIRMWARE_MODULE_IDENTITY = IGVC_MODULE_TYPE;
 
 void app_main()
 {
-    base_set_state_bl();
-
     tasking_init();
     modules_init();
 
     tasking_begin();
+
+    base_set_state_bl();
 }
