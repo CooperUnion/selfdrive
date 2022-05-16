7a3c83bbdba4869bcd86e9c290ff1f5bdbff0d16
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-16 18:22:30 -0400

    Register magic packet

--- dbw/node_fw/src/sys/bl_tools.c
+++ dbw/node_fw/src/sys/bl_tools.c
@@ -3,23 +3,42 @@
 #include <esp_ota_ops.h>
 
 #include "common.h"
+#include "io/can.h"
+#include "module_types.h"
 #include "sys/task_glue.h"
 
 // ######        DEFINES        ###### //
 
 // ######      PROTOTYPES       ###### //
 
+static void bl_tools_init();
 static void bl_tools_100Hz();
 
 // ######     PRIVATE DATA      ###### //
 
 // ######          CAN          ###### //
 
+static struct CAN_dbwBL_Magic_Packet_t CAN_BL_Magic_Packet;
+
+static can_incoming_t can_BL_Magic_Packet_cfg = {
+    .id = CAN_DBWBL_MAGIC_PACKET_FRAME_ID,
+    .out = &CAN_BL_Magic_Packet,
+    .unpack = CAN_dbwBL_Magic_Packet_unpack,
+};
+
 // ######    RATE FUNCTIONS     ###### //
 
 const struct rate_funcs bl_tools_rf = {
+    .call_init  = bl_tools_init,
     .call_100Hz = bl_tools_100Hz,
 };
+
+static void bl_tools_init()
+{
+    can_BL_Magic_Packet_cfg.id += FIRMWARE_MODULE_IDENTITY;
+
+    can_register_incoming_msg(can_BL_Magic_Packet_cfg);
+}
 
 static void bl_tools_100Hz()
 {
