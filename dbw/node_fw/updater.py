7c1936e2068fafd61d72453bca59270f8fb001f0
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-19 12:15:09 -0400

    Add argument parsing

--- dbw/node_fw/updater.py
+++ dbw/node_fw/updater.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
+import argparse
+
+
 def main():
-    pass
+    argparser = argparse.ArgumentParser(description='node_fw updater')
+
+    argparser.add_argument(
+        '-m',
+        '--module',
+        help='module to flash',
+    )
+
+    args = argparser.parse_args()
 
 
 if __name__ == '__main__':
