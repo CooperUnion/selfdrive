2c1a4d302745975320497abf2a7c54da4cf31910
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-20 12:25:48 -0400

    Initialize bootloader timer

--- dbw/node_bl/src/sys/bl.c
+++ dbw/node_bl/src/sys/bl.c
@@ -1,14 +1,19 @@
 #include "sys/bl.h"
 
+#include <driver/timer.h>
 #include <esp_err.h>
 #include <esp_ota_ops.h>
 #include <esp_system.h>
+
+#include "io/can.h"
 
 // ######        DEFINES        ###### //
 
 // ######      PROTOTYPES       ###### //
 
 // ######     PRIVATE DATA      ###### //
+
+static uint64_t bl_init_timer_val;
 
 static const esp_partition_t *ota_partition;
 
@@ -20,12 +25,17 @@ static const esp_partition_t *ota_partition;
 
 esp_err_t bl_init(void)
 {
+    esp_err_t err;
+
     ota_partition = esp_partition_find_first(
         ESP_PARTITION_TYPE_APP,
         ESP_PARTITION_SUBTYPE_APP_OTA_0,
         NULL
     );
     if (!ota_partition) return ESP_FAIL;
+
+    err = timer_get_counter_value(CAN_TIMER_GROUP, CAN_TIMER, &bl_init_timer_val);
+    if (err != ESP_OK) return err;
 
     return ESP_OK;
 }
