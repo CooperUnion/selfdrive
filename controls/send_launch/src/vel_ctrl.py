0a3da85300141ee731566fb14182fa2928ceeda6
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-23 17:33:59 -0400

    Added CAN listener thread for filtering incoming encoder data

--- controls/send_launch/src/vel_ctrl.py
+++ controls/send_launch/src/vel_ctrl.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+# ./vel_ctrl.py --kp 1.3 --ki 0.015 --kd 1.35 --vd 2
 import argparse
 import time
 import numpy as np
 from cand.client import Bus
 from PID_beard import PIDController
+
+from threading import Thread
 
 #from geometry_msgs.msg import Twist
 
@@ -12,8 +15,31 @@ class vel_ctrl:
     def __init__(self, kp=0.0, ki=0.0, kd=0.0, Ts=0.1):
         self.controller = PIDController(kp=kp, ki=ki, kd=kd, Ts=Ts)
         self.bus = Bus()
-        self.vel_history = np.zeros(10)
-        self.index = 0
+        self.vel_filtered = 0
+        self.N            = 4
+        self.vel_history = np.zeros(self.N)
+        self.candListener = Thread(target=self.vel_filter, daemon=True)
+        self.candListener.start()
+
+    def vel_filter(self):
+
+        prev_time = 0
+        index = 0
+
+        while True:
+            data = self.bus.get('dbwNode_Encoder_Data')
+
+            if prev_time == data[0]:
+                continue
+
+            prev_time = data[0]
+
+            self.vel_history[index] = vel_ctrl.enc_to_velocity((data[1]['Encoder0'] + data[1]['Encoder1']) / 2, 0.01)
+            vel_in = self.vel_history[index]
+            index = (index + 1) % self.N
+
+            self.vel_filtered = np.mean(self.vel_history)
+
     def test_connection(self, twist_message, ctrl_out):
         v_des = twist_message.linear.x
         pedal_percentage = v_des/100
@@ -35,17 +61,16 @@ class vel_ctrl:
 #        self.bus.send('dbwNode_Accel_Cmd', {'ThrottleCmd': max(pedal_percentage, 0) / 100, 'ModeCtrl': 1})
 
     def ctrl_vel_fixed(self, v_des=2.2352):
-        enc = self.bus.get('dbwNode_Encoder_Data')
-        delta = (time.time_ns() - enc[0])/1_000_000_000
-        data = enc[1]
-        v_actual = self.enc_to_velocity(data, delta)
-        self.vel_history[self.index] = v_actual
-        self.index = (self.index + 1) % 10
-        acceleration_desired = self.controller.PID(v_des, np.mean(self.vel_history))
-        (throttle_percentage, brake_percentage) = self.brake_or_throttle(v_des, np.mean(self.vel_history), acceleration_desired)
+        #enc = self.bus.get('dbwNode_Encoder_Data')
+        #delta = (time.time_ns() - enc[0])/1_000_000_000
+        #data = enc[1]
+        v_actual = self.vel_filtered
+        acceleration_desired = self.controller.PID(v_des, self.vel_filtered)
+        (throttle_percentage, brake_percentage) = self.brake_or_throttle(v_actual, acceleration_desired)
+        print(throttle_percentage)
         self.bus.send("dbwNode_SysCmd", {"DbwActive": 1, "ESTOP": 0})
-        self.bus.send('dbwNode_Accel_Cmd', {'ThrottleCmd': max(throttle_percentage, 0) / 100, 'ModeCtrl': 1})
-        self.bus.send('dbwNode_Brake_Cmd', {'BrakeCmd': max(brake_percentage, 0) / 100, 'ModeCtrl': 1})
+        self.bus.send('dbwNode_Accel_Cmd', {'ThrottleCmd': min(max(throttle_percentage, 0), 100) / 100, 'ModeCtrl': 1})
+        self.bus.send('dbwNode_Brake_Cmd', {'BrakeCmd': min(max(brake_percentage, 0), 100) / 100})
 
     def acc_to_pedal(self, acceleration):
         return 15.4*acceleration
@@ -53,11 +78,12 @@ class vel_ctrl:
     def signal_processing(self, v_sample):
         pass
 
-    def enc_to_velocity(self, enc, time):
+    @staticmethod
+    def enc_to_velocity(ticks, time):
         enc_ticks = 4000
         wheel_circumference = 1.899156
         meters_per_tick = wheel_circumference/enc_ticks
-        return (meters_per_tick * enc['Encoder0'])/time
+        return (meters_per_tick * ticks)/time
 
     def brake_to_pedal(self, brake):
         return (-58.03*brake)-11.33
@@ -77,7 +103,7 @@ class vel_ctrl:
     #             return (self.acc_to_pedal(acceleration_desired), 0)
     #         elif(acceleration_desired<0):
     #             return(0, self.brake_to_pedal(acceleration_desired))
-    def brake_or_throttle(self,v_actual, acceleration_desired):
+    def brake_or_throttle(self, v_actual, acceleration_desired):
         if(v_actual<0):
             return(0, 60)
         elif(v_actual>= 0):
