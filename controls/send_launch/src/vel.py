b5e01b01eed8444e42a2e17b415a7819cf335c52
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-26 02:01:38 -0400

    Define `Ctrl._tick2vel()`

--- controls/send_launch/src/vel.py
+++ controls/send_launch/src/vel.py
@@ -6,6 +6,7 @@ import cand
 class Ctrl:
     ENCODER_TICKS_PER_ROTATION = 4000
     WHEEL_CIRCUMFRANCE_M       = 1.899156
+    TICKS_PER_M                = WHEEL_CIRCUMFRANCE_M / ENCODER_TICKS_PER_ROTATION
 
     ENCODER_CAN_MESSAGE_NAME = 'dbwNode_Encoder_Data'
     ENOCDER0_SIGNAL_NAME     = 'Encoder0'
@@ -15,3 +16,6 @@ class Ctrl:
     def __init__(self, pid: pid.Controller, bus: cand.client.Bus):
         self._pid = pid
         self._bus = bus
+
+    def _tick2vel(self, ticks: int, time: int) -> float:
+        return self.TICKS_PER_M * ticks / time
