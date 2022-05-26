b43153cce140ab8cd7b80b77fae28b44c1baeeb7
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-26 19:37:55 -0400

    Decode vehicular velocity

--- controls/send_launch/src/twist.py
+++ controls/send_launch/src/twist.py
@@ -7,8 +7,10 @@ class Decode:
     def __init__(self, topic: str):
         self._topic = rospy.Subscribe(topic, Twist, self._callback)
 
+        self.vel = 0.0
+
     def _callback(self, msg: Twist):
-        pass
+        vel = msg.linear.x
 
 
 class TwistCan:
