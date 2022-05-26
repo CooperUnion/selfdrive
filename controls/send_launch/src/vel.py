b0adbbdce128d67fc58aed96633790f789af235d
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-26 16:03:59 -0400

    Define mapping functions

--- controls/send_launch/src/vel.py
+++ controls/send_launch/src/vel.py
@@ -8,6 +8,10 @@ class Ctrl:
     WHEEL_CIRCUMFRANCE_M       = 1.899156
     TICKS_PER_M                = WHEEL_CIRCUMFRANCE_M / ENCODER_TICKS_PER_ROTATION
 
+    ACCEL_TO_PEDAL_SLOPE_MAPPING           = 15.4
+    BRAKE_TO_PEDAL_SLOPE_MAPPING           = -58.03
+    BRAKE_TO_PEDAL_SLOPE_MAPPING_INTERCEPT = -11.33
+
     ENCODER_CAN_MESSAGE_NAME = 'dbwNode_Encoder_Data'
     ENOCDER0_SIGNAL_NAME     = 'Encoder0'
     ENOCDER1_SIGNAL_NAME     = 'Encoder1'
@@ -16,6 +20,12 @@ class Ctrl:
     def __init__(self, pid: pid.Controller, bus: cand.client.Bus):
         self._pid = pid
         self._bus = bus
+
+    def _accel2pedal(self, accel: float) -> float:
+        return self.ACCEL_TO_PEDAL_SLOPE_MAPPING * accel
+
+    def _brake2pedal(self, accel: float) -> float:
+        return (self.BRAKE_TO_PEDAL_SLOPE_MAPPING * accel) - self.BRAKE_TO_PEDAL_SLOPE_MAPPING_INTERCEPT
 
     def _tick2vel(self, ticks: int, time: int) -> float:
         return self.TICKS_PER_M * ticks / time
