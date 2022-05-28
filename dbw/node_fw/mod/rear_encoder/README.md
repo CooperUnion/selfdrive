b88d7a6cb1327d7c371c5e06b0d2408c2824affa
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-28 18:13:57 +0000

    DBW-57 rear encoders

---
+++ dbw/node_fw/mod/rear_encoder/README.md
@@ -0,0 +1,3 @@
+> 2022-05-28
+
+Generalize encoders at a later date.
--- dbw/node_fw/src/module_types.h
+++ dbw/node_fw/src/module_types.h
@@ -9,6 +9,7 @@ enum firmware_module_types {
     MOD_THROTTLE = 2,
     MOD_BRAKE = 3,
     MOD_ENCODER = 4,
+    MOD_REAR_ENCODER = 5,
 };
 
 extern const enum firmware_module_types FIRMWARE_MODULE_IDENTITY;
