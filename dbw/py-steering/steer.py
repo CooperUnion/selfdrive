3848ee43f9015b06094dacfd718d3a7ba2b154f1
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-29 11:09:00 -0400

    Define `Steer._enc2angle()`

--- dbw/py-steering/steer.py
+++ dbw/py-steering/steer.py
@@ -5,8 +5,14 @@ import igvcutils
 
 
 class Steer(threading.Thread):
+    ENCODER_TO_ANGLE_SLOPE_MAPPING           = 0.0029
+    ENCODER_TO_ANGLE_SLOPE_MAPPING_INTERCEPT = 0.0446
+
     def __init__(self, bus: cand.client.Bus, pid: igvcutils.ctrl.Pid):
         self._bus = bus
         self._pid = pid
 
         threading.Thread.__init__(self, daemon=True)
+
+    def _enc2angle(self, val: int) -> float:
+        return (self.ENCODER_TO_ANGLE_SLOPE_MAPPING * val) + self.ENCODER_TO_ANGLE_SLOPE_MAPPING_INTERCEPT
