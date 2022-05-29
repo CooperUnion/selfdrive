1907011d5fc2682ec3a59cee9855b0be84c8c11f
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-29 09:23:16 -0400

    Add necessary program arguments

--- dbw/py-steering/py-steering.py
+++ dbw/py-steering/py-steering.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python3
+import argparse
+
+import cand
+
 
 def main():
-    pass
+    argparser = argparse.ArgumentParser(description='py-steering')
+    argparser.add_argument(
+        '--redis-host',
+        default='localhost',
+        help='redis hostname',
+        metavar='hostname',
+    )
+    argparser.add_argument(
+        '--redis-port',
+        default='6379',
+        help='redis port',
+        metavar='6379',
+    )
+
+    args = argparser.parse_args()
+
+    bus = cand.client.Bus(
+        redis_host=args.redis_host,
+        redis_port=args.redis_port,
+    )
 
 
 if __name__ == '__main__':
