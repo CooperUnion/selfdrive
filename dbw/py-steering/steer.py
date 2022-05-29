a9ec5a0da6b4e6846c4f9fbc97bc2539cbf8c110
Jacob Koziej <jacobkoziej@gmail.com>
2022-05-29 11:15:17 -0400

    Include base for steer

--- dbw/py-steering/steer.py
+++ dbw/py-steering/steer.py
@@ -3,14 +3,22 @@ import threading
 import cand
 import igvcutils
 
+import base
+
 
 class Steer(threading.Thread):
     ENCODER_TO_ANGLE_SLOPE_MAPPING           = 0.0029
     ENCODER_TO_ANGLE_SLOPE_MAPPING_INTERCEPT = 0.0446
 
-    def __init__(self, bus: cand.client.Bus, pid: igvcutils.ctrl.Pid):
-        self._bus = bus
-        self._pid = pid
+    def __init__(
+        self,
+        bus:  cand.client.Bus,
+        pid:  igvcutils.ctrl.Pid,
+        base: base.Base
+    ):
+        self._bus  = bus
+        self._pid  = pid
+        self._base = base
 
         threading.Thread.__init__(self, daemon=True)
 
