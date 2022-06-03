ad9fc690814439890ca4f033a0fb923adf2c5fda
Jacob Koziej <jacobkoziej@gmail.com>
2022-06-03 08:50:32 -0400

    Undo naming crimes

--- controls/send_launch/src/vel.py
+++ controls/send_launch/src/vel.py
@@ -11,20 +11,6 @@ class Ctrl:
     WHEEL_CIRCUMFRANCE_M       = 1.899156
     TICKS_PER_M                = WHEEL_CIRCUMFRANCE_M / ENCODER_TICKS_PER_ROTATION
 
-    ACCEL_TO_PEDAL_SLOPE_MAPPING           = 15.4
-    BRAKE_TO_PEDAL_SLOPE_MAPPING           = -58.03
-    BRAKE_TO_PEDAL_SLOPE_MAPPING_INTERCEPT = -11.33
-
-    ACCEL_CAN_MESSAGE_NAME    = 'dbwNode_Accel_Cmd'
-    THROTTLE_SIGNAL_NAME      = 'ThrottleCmd'
-    MODE_CTRL_SIGNAL_NAME     = 'ModeCtrl'
-    BRAKE_CAN_MESSAGE_NAME    = 'dbwNode_Brake_Cmd'
-    BRAKE_SIGNAL_NAME         = 'BrakeCmd'
-    ENCODER_CAN_MESSAGE_NAME  = 'dbwNode_Encoder_Data'
-    ENOCDER0_SIGNAL_NAME      = 'Encoder0'
-    ENOCDER1_SIGNAL_NAME      = 'Encoder1'
-    TIME_DETLA_US_SIGNAL_NAME = 'Time'
-
     def __init__(
         self,
         pid: pid.Controller,
@@ -48,25 +34,25 @@ class Ctrl:
         self._bus_thread.start()
 
     def _accel2pedal(self, accel: float) -> float:
-        return self.ACCEL_TO_PEDAL_SLOPE_MAPPING * accel
+        return self.ACCEL_TO_PEDAL_MAPPING * accel
 
     def _brake2pedal(self, accel: float) -> float:
-        return (self.BRAKE_TO_PEDAL_SLOPE_MAPPING * accel) + self.BRAKE_TO_PEDAL_SLOPE_MAPPING_INTERCEPT
+        return (self.BRAKE_TO_PEDAL_MAPPING * accel) + self.BRAKE_TO_PEDAL_MAPPING_OFFSET
 
     def _tick2vel(self, ticks: int, time: int) -> float:
         return self.TICKS_PER_M * ticks / time
 
     def _vel_filter(self):
         while True:
-            reading = self._bus.get(self.ENCODER_CAN_MESSAGE_NAME)
+            reading = self._bus.get('dbwNode_Encoder_Data')
             if not reading: continue
 
             if self._vel_prv_time == reading[0]: continue
             self._vel_prv_time = reading[0]
 
             self._vel_hist[self._vel_index] = self._tick2vel(
-                (reading[1][self.ENOCDER0_SIGNAL_NAME] + reading[1][self.ENOCDER1_SIGNAL_NAME]) / 2,
-                reading[1][self.TIME_DETLA_US_SIGNAL_NAME] / 1_000_000,
+                (reading[1]['Encoder0'] + reading[1]['Encoder1']) / 2,
+                reading[1]['Time'] / 1_000_000,
             )
 
             self._vel_index = (self._vel_index + 1) % self._samples
@@ -83,13 +69,10 @@ class Ctrl:
         if vel <= 0.0: accel = 0.0
 
         self._bus.send(
-            self.ACCEL_CAN_MESSAGE_NAME,
-            {
-                self.THROTTLE_SIGNAL_NAME:  min(abs(accel), 100) / 100,
-                self.MODE_CTRL_SIGNAL_NAME: 1,
-            },
+            'dbwNode_Accel_Cmd',
+            {'ThrottleCmd': min(abs(accel), 100) / 100, 'ModeCtrl': 1},
         )
         self._bus.send(
-            self.BRAKE_CAN_MESSAGE_NAME,
-            {self.BRAKE_SIGNAL_NAME: min(abs(brake), 100) / 100},
+            'dbwNode_Brake_Cmd',
+            {'BrakeCmd': min(abs(brake), 100) / 100},
         )
