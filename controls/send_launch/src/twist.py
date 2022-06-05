05dc6217b7cc1ad79dcd6ff4b522ba8e9336d65b
Jacob Koziej <jacobkoziej@gmail.com>
2022-06-05 17:28:36 -0400

    Decode steering angle

--- controls/send_launch/src/twist.py
+++ controls/send_launch/src/twist.py
@@ -7,7 +7,9 @@ class Decode:
     def __init__(self, topic: str):
         self._topic = rospy.Subscriber(topic, Twist, self._callback)
 
-        self.vel = 0.0
+        self.angle = 0.0
+        self.vel   = 0.0
 
     def _callback(self, msg: Twist):
-        vel = msg.linear.x
+        angle = msg.angular.z
+        vel   = msg.linear.x
