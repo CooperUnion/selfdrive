10432b186f3259c5f174cf98e68dce01016396ee
Azra Rangwala <azra.rangwala@dbw-prada.host.autonomylab.nyc>
2022-12-08 12:51:32 -0500

    pure_pursuit.py

--- ros/docker/pure_pursuit/pure_pursuit_launch/src/pure_pursuit.py
+++ ros/docker/pure_pursuit/pure_pursuit_launch/src/pure_pursuit.py
@@ -6,20 +6,24 @@ from nav_msgs.msg import Path
 
 class Subscribe:
     def __init__(self):
-        self.sub_twist = rospy.Subscriber('/cmd_vel', Twist, self.callback)
+        self.sub_twist = rospy.Subscriber('/cmd_vel', Twist, self.callback) #orange line is the node
+        #subscribing to twist message from cmd_vel
         self.sub_path = rospy.Subscriber('/global_planner/plan', Path, self.callback)
+        #subscribing to global_planner node
     def callback(msg):
         while True:
-            rospy.loginfo("I heard %s", msg.data)
+            rospy.loginfo("I heard %s", msg.data) 
 
 class Publish:
     def __init__(self):
         self.pose = pose
         self.pub_twist = rospy.Publisher('/pure_pursuit/twist', Twist, queue_size=2)
+        #high level controls publishes our own twist message
         self.rate = rospy.Rate(2)
     def publish(self,pose):
         while not rospy.is_shutdown():
             self.pub_twist.publish(pose)
+            #publishing the pose
     
 class Controller:
     def __init__(self):
