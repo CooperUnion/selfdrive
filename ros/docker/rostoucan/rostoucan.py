fbbd962759d9182f64a92a97e4fbb840fd6a75a2
Lizelle Ocfemia0 <lizelle.ocfemia@cooper.edu>
2023-01-22 15:30:01 -0500

    add rostoucan and cantouros

--- ros/docker/rostoucan/rostoucan.py
+++ ros/docker/rostoucan/rostoucan.py
@@ -1,5 +1,25 @@
+#!/usr/bin/env python3
+
 import rospy
 from geometry_msgs.msg import Twist
 
-import cand 
+import cand
+
+CANmessage = 'DBW_VelocityCmd'
+ROStopic = '/cmd_vel'
+
+class ROStouCAN:
+    def __init__(self):
+        self.bus = cand.client.Bus()
+        rospy.Subscriber(ROStopic, Twist, self.callback)
+    def callback(self, msg):
+        self.bus.send(CANmessage, {
+            'linearVelCmd': msg.linear.x,
+            'angularVelCmd': msg.angular.z
+        })
+    
+if __name__ == '__main__':
+    rospy.init_node('roscan!', anonymous=True)
+    ROStouCAN()
+    rospy.spin()
 
