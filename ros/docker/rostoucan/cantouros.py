b23a19fc63dc5ef368809ef88ac9f5bdca3486d4
Lizelle Ocfemia0 <lizelle.ocfemia@cooper.edu>
2023-01-26 19:28:16 -0500

    add comments on 'rostoucan.py'

--- ros/docker/rostoucan/cantouros.py
+++ ros/docker/rostoucan/cantouros.py
@@ -14,10 +14,10 @@ class CANtouROS:
         self.bus = cand.client.Bus()
         pub = rospy.Publisher(ROStopic, Twist, self.callback)
     def callback(self,msg):
-        self.bus.send(CANmessage, {
-            'linearVelCmd': 1.0,
-            'angularVelCmd': 1.0
-        })
+        # self.bus.send(CANmessage, {
+        #     'linearVelCmd': 1.0,
+        #     'angularVelCmd': 1.0
+        # })
         data_rec = self.bus.get(CANmessage)
         data_time, data_data = data_rec
         msg.linear.x = data_data['linearVelCmd']
@@ -26,7 +26,7 @@ class CANtouROS:
         
 
 if __name__ == '__main__':
-    rospy.init_node('canros?', anonymous=True)
+    rospy.init_node('canros', anonymous=True)
     CANtouROS()
     rospy.spin()
 
--- ros/docker/rostoucan/rostoucan.py
+++ ros/docker/rostoucan/rostoucan.py
@@ -17,9 +17,18 @@ class ROStouCAN:
             'linearVelCmd': msg.linear.x,
             'angularVelCmd': msg.angular.z
         })
+        while True:
+            rospy.loginfo("I heard %s", msg.linear.x)
     
 if __name__ == '__main__':
     rospy.init_node('roscan!', anonymous=True)
     ROStouCAN()
     rospy.spin()
 
+# sudo apt install python3.9
+# python3.9 -m pip install opencan-cand
+
+# start redis container
+# set up vcan on local machine sudo modprobe vcan
+# start cand on local machine candump vcan0
+# redis local host flag on cand --redis_host localhost
