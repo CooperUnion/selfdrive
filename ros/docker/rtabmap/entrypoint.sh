49c3df721cd8c00276cf14d8dbf86316b8fa8372
Giuseppe Quaratino <giuseppe.quaratino@cooper.edu>
2023-02-13 16:53:41 -0500

    fixed alias commands in rtabmap Dockerfile

--- ros/docker/rtabmap/entrypoint.sh
+++ ros/docker/rtabmap/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 # source setup environments for ros and catkin workspace
 source /opt/ros/noetic/setup.bash
-#source /app/ros_ws/devel/setup.bash
+source /app/ros_ws/devel/setup.bash
 
 # https://unix.stackexchange.com/questions/466999/what-does-exec-do
 exec "$@"
