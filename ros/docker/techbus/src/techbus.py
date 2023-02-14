e7d02d2a423a4291c92527f5b9dd87013d8c6c95
Giuseppe Quaratino <giuseppe.quaratino@cooper.edu>
2023-02-13 23:13:18 -0500

    slight change to techbus, requires testing

--- ros/docker/techbus/src/techbus.py
+++ ros/docker/techbus/src/techbus.py
@@ -19,20 +19,19 @@ class ROStouCAN:
         self.bus = cand.client.Bus(redis_host='redis')
         rospy.Subscriber(PPTwist, Twist, self.callback)
     def callback(self, msg):
-        while True:
+        angle = 0
+        if msg.linear.x != 0 and msg.angular.z != 0:
+            angle = -np.arctan(msg.angular.z * 1.8 / msg.linear.x)
+        else:
+            rospy.loginfo(f"Warning: invalid steering angle combo:lin.x:{msg.linear.x}, ang.z:{msg.angular.z}")
             angle = 0
-            if msg.linear.x != 0 and msg.angular.z != 0:
-                angle = -np.arctan(msg.angular.z * 1.8 / msg.linear.x)
-            else:
-                rospy.loginfo(f"Warning: invalid steering angle combo:lin.x:{msg.linear.x}, ang.z:{msg.angular.z}")
-                angle = 0
-            rospy.loginfo("I heard %s", msg.linear.x)
-            self.bus.send(VelocityCAN, {
-                'DBW_linearVelCmd': msg.linear.x
-            })
-            self.bus.send(SteerCAN, {
-                'STEER_angleCmd': angle
-            })
+        rospy.loginfo("I heard %s", msg.linear.x)
+        self.bus.send(VelocityCAN, {
+            'DBW_linearVelCmd': msg.linear.x
+        })
+        self.bus.send(SteerCAN, {
+            'STEER_angleCmd': angle
+        })
 
 class CANtouROS:
     def __init__(self):
@@ -41,20 +40,20 @@ class CANtouROS:
         self.pub = rospy.Publisher(EncoderTicks, UInt16MultiArray, queue_size=2)
         self.rate = rospy.Rate(1)
     def publish(self):
-        while not rospy.is_shutdown():
-            data = self.bus.get_data(EncoderCAN)
-            if data is None:
-                continue
-            self.msg.data = [data['CTRL_encoderLeft'], data['CTRL_encoderRight']]
-            self.pub.publish(self.msg)
-        
+        data = self.bus.get_data(EncoderCAN)
+        if data is None:
+            continue
+        self.msg.data = [data['CTRL_encoderLeft'], data['CTRL_encoderRight']]
+        self.pub.publish(self.msg)
+        self.rate.sleep()
+
 if __name__ == '__main__':
     rospy.init_node('roscan', anonymous=True)
     can_publisher = ROStouCAN()
     ros_publisher = CANtouROS()
-    ros_publisher.publish()
-    rospy.spin()
-
+    while not rospy.is_shutdown():
+        ros_publisher.publish()
+        
 # sudo apt install python3.9
 # python3.9 -m pip install opencan-cand
 
