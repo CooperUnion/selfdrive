3a1ab2ba65c6231e7bc1228cf4cda91a7a5b2423
Vaibhav Hariani <vaibhav.hariani@cooper.edu>
2023-03-01 14:52:13 -0500

    Modified arduino_spin to account for two motors

--- arduino_spin.py
+++ arduino_spin.py
@@ -1,21 +1,31 @@
 import pyfirmata
 import time
-angvel = 60 ##In RPM
-steps = 200 
-t = 30/(angvel*steps)
-dir = 1
-if __name__ == "__main__":
-    board = pyfirmata.ArduinoMega("/dev/ttyACM0")
-    print('howdy')
-    board.digital[5].write(dir)
+import math
+
+board = pyfirmata.ArduinoMega('/dev/ttyACM0')
+RPS = 360/200 #Number of Pulses per Rev, Modify on Motor Driver, Then Here
+WHEEL_RADIUS = 0.5 #Units: Meters, Modify Based on Wheel
+Forward = True #Controlled by CMD_Vel
+Lin_Vel = [0,0] #M/S for each wheel, Controlled by CMD_Vel
+angvel = [i/WHEEL_RADIUS for i in Lin_Vel] #Rad/Sec
+dps = [i*180/math.pi for i in Lin_Vel] #Degrees/Sec
+sbs = [RPS/(2*i) for i in dps] #Steps/Sec
+motor1 = board.digital[11]
+motor2 = board.digital[12]
+if __name__ == '__main__':
+    print('booted')
     while True:
-        board.digital[11].write(1)
-        time.sleep(t)
-        board.digital[11].write(0)
-        time.sleep(t)
-
-
-
-
-
-
+        if Forward:
+            board.digital[5].write(1)
+        else:
+            board.digital[5].write(0)
+        Forward = True
+        Lin_Vel = [0,0]
+        angvel = [i/WHEEL_RADIUS for i in Lin_Vel]
+        dps = [i*180/math.pi for i in Lin_Vel]
+        sbs = [RPS/(2*i) for i in dps]
+         
+        motor1.write(1)
+        time.sleep(sbs)
+        motor1.write(0)
+        time.sleep(sbs)
