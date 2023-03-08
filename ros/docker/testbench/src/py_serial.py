a2cea3cd28f337098642b8a16b7badcc041b0231
Vaibhav Hariani <vaibhav.hariani@cooper.edu>
2023-03-08 14:57:36 -0500

    Attempted to turn py_serial into subscriber node

--- ros/docker/testbench/src/py_serial.py
+++ ros/docker/testbench/src/py_serial.py
@@ -1,31 +1,43 @@
 import serial
-import time
 import math
+import rospy
+from std_msgs.msg import twist
+
 arduino = serial.Serial(port='/dev/ttyACM0',baudrate=9600,timeout=1)
 RPS = 200 #Number of Pulses per Rev, Modify on Motor Driver, Then Here
-
+WIDTH = 5 # Meters
 WHEEL_RADIUS = 0.5 #Units: Meters, Modify Based on Wheel
-Lin_Vel = [5,5] #Signed m/s for each wheel, Controlled by CMD_Vel
 
-sbs = [0,0] #Definition of 0 speed, needed a value for that
-for i in range(len(Lin_Vel)):
-    if Lin_Vel[i] == 0:
-        sbs[i] = '0'
-    else:
-        ang_vel = Lin_Vel[i]/(WHEEL_RADIUS*2*math.pi) #revolutions/sec
-        s = (2*(10**6))/(ang_vel * RPS) 
-        sbs[i] = str(int(s)) #seconds/sec, dividing by two for positive and negative edges
+def micros_between(vel):
+    micros = ['0','0']
+    for i in range(0,len(vel)):
+        if vel[i] != 0:
+            steps_per = vel[i] * RPS
+            seconds_between = 1/steps_per
+            micros[i] = str(int(seconds_between))
+    return micros
 
-def send(msg):
+def send(msg):        
     arduino.write(bytes(msg,'utf-8'))
     return
 
 
-if __name__ == '__main__':
-    print('booted')
-    while True:
-        #msg = sbs[0] + " " + sbs[1] + "\n"
-        msg = "200 200 \n"
+class Subscriber:
+   
+    def __init__(self):
+        rospy.Subscriber('/vector',twist,self.callback)
 
+    def callback(self,msg):
+        v = msg.linear.x
+        w = msg.angular.z
+        vel = [((2 * v) + (w * WIDTH)) / (4 * WHEEL_RADIUS * math.pi),
+            ((2 * v) - (w * WIDTH)) / (4 * WHEEL_RADIUS * math.pi)] 
+        micros = micros_between(vel)
+        msg = micros[0] + " " + micros[1] + "\n"
         send(msg)
-        time.sleep(1)
+
+
+if __name__ == '__main__':
+    rospy.init_node
+    sub = Subscriber()
+    rospy.spin()
