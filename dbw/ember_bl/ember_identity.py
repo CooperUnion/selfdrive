eb1df05f950f64eaf5bdfce517c86aa9782367f0
Dan Mezhiborsky <daniel.mezhiborsky@cooper.edu>
2023-03-27 11:31:45 -0400

    Move ember_app_desc.c and ember_identity.py to ember

--- dbw/ember_bl/ember_identity.py
+++
@@ -1,6 +0,0 @@
-# Simple script to add a #define for EMBER_NODE_IDENTITY.
-
-Import('env')
-
-identity = env.GetProjectOption('board_node_identity')
-env.Append(CPPDEFINES=[("EMBER_NODE_IDENTITY", f'\\\"{identity}\\\"')])
--- dbw/node_fw/src/app_desc.c
+++
@@ -1,8 +0,0 @@
-#include "ember_app_desc.h"
-
-const IN_DESC_SECTION ember_app_desc_v1_t ember_app_description = {
-    .ember_magic = EMBER_MAGIC,
-    .app_desc_version = EMBER_APP_DESC_VERSION,
-
-    .node_identity = "TESTBL",
-};
