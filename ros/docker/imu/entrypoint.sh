14c5c84716655c29d4278ec3842eb04c80a8238c
Giuseppe Quaratino <giuseppe.quaratino@cooper.edu>
2023-03-29 17:03:07 -0400

    add imu_tools package

--- ros/docker/imu/entrypoint.sh
+++ ros/docker/imu/entrypoint.sh
@@ -6,5 +6,9 @@ set -e
 # source setup environments for ros and catkin workspace
 source /opt/ros/noetic/setup.bash
 
+# cd ros_ws/src/imu/kvh1750_ws/
+# catkin_make
+# source devel/setup.bash
+
 # https://unix.stackexchange.com/questions/466999/what-does-exec-do
 exec "$@"
