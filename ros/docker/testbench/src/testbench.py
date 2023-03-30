823e6eb186ed77896a74a9cf8d9bdd858e3bebe2
Vaibhav Hariani <vaibhav.hariani@cooper.edu>
2023-03-30 13:31:32 -0400

    Dockerized and Roserized testbench.py

--- ros/docker/testbench/src/testbench.py
+++ ros/docker/testbench/src/testbench.py
@@ -2,6 +2,9 @@ import serial
 import math
 import rospy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import UInt16MultiArray, String
+EncoderTicks = '/encoder_ticks'
+
 arduino = serial.Serial(port='/dev/ttyACM0',baudrate=9600,timeout=1)
 RPS = 200 #Number of Pulses per Rev, Modify on Motor Driver, Then Here
 WIDTH = 5 # Meters
@@ -19,23 +22,39 @@ def send(msg):
     arduino.write(bytes(msg,'utf-8'))
     return
 
+class Publisher:
+    def __init__(self):
+        self.msg = UInt16MultiArray()
+        self.pub = rospy.Publisher(EncoderTicks, UInt16MultiArray, queue_size=2)
+        self.rate = rospy.Rate(50)
 
-class Subscriber:
-   
+    def publish(self):
+        str = arduino.readline()
+        if str is None:
+            return
+        data = str.split('#')
+        self.msg.data = [int(data[0]),int(data[1])]
+        self.pub.publish(self.msg)
+        self.rate.sleep()
+    
+
+class Subscriber:   
     def __init__(self):
         rospy.Subscriber('/twist',Twist,self.callback)
-    
     def callback(self,msg):
         v = msg.linear.x
         w = msg.angular.z
         vel = [((2 * v) + (w * WIDTH)) / (4 * WHEEL_RADIUS * math.pi),
             ((2 * v) - (w * WIDTH)) / (4 * WHEEL_RADIUS * math.pi)] 
         micros = micros_between(vel)
-        msg = micros[0] + " " + micros[1] + "\n"
+        msg = micros[0] + "#" + micros[1] + "\n"
         send(msg)
 
 
 if __name__ == '__main__':
     rospy.init_node('subscriber', anonymous=True)
     sub = Subscriber()
+    pub = Publisher()
+    while not rospy.is_shutdown():
+        pub.publish()
     rospy.spin()
--- ros/scripts/ad/ad.ino
+++ ros/scripts/ad/ad.ino
@@ -2,10 +2,13 @@ int m1sig = 0;
 int m2sig = 0;
 unsigned long previousMicros1=0;
 unsigned long previousMicros2=0;
+short L_ticks = 0;
+short R_ticks = 0;
+
 
 
 void setup() {
-  Serial.begin(9600);   // opens serial port, sets data rate to 9600 bps
+  Serial.begin(9600);  // opens serial port, sets data rate to 9600 bps
   pinMode(13, OUTPUT); //MOTOR1_PULSE
   pinMode(12, OUTPUT); //MOTOR2_PULSE
   pinMode(11, OUTPUT); //MOTOR1_DIR
@@ -18,13 +21,9 @@ void loop() {
 if(Serial.available() > 0){
   String inp = Serial.readStringUntil('\n');
   inp.trim();               
-  int n = inp.indexOf(" ");
+  int n = inp.indexOf("#");
   m1sig = inp.substring(0,n).toInt();
-    Serial.print("m1sig : ");
-    Serial.println(m1sig);
   m2sig = inp.substring(n).toInt();
-    Serial.print("m2sig : ");
-    Serial.println(m2sig);
 
   if(m1sig < 0) {
     digitalWrite(11, LOW);
@@ -45,10 +44,15 @@ if(Serial.available() > 0){
   if(m1sig != 0 && (unsigned long) (currentMicros - previousMicros1) >= (unsigned long) m1sig){
     digitalWrite(13,!digitalRead(13));
     previousMicros1=currentMicros;
+    L_ticks++;
+    Serial.println(L_ticks,"#",R_ticks);
   }
   
   if(m2sig !=  0 && (unsigned long) (currentMicros - previousMicros2) >= (unsigned long) m2sig){
     digitalWrite(12,!digitalRead(12));
-    previousMicros2=currentMicros;  
+    previousMicros2=currentMicros; 
+    R_ticks++; 
+    Serial.println(L_ticks,"#",R_ticks);
+
   }
 }
