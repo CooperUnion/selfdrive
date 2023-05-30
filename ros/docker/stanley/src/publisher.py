dc29082ba2e574b3139d5c6be187e3b674d917a7
Giuseppe Quaratino <giuseppe.quaratino@cooper.edu>
2023-05-29 22:16:20 -0400

    integrate stanley controller with ros

--- ros/docker/stanley/src/publisher.py
+++ ros/docker/stanley/src/publisher.py
@@ -8,20 +8,30 @@ from std_msgs.msg import Float32MultiArray
 from subscriber import Subscriber
 from controller import StanleyController
 
+# Max distance from goal point set in meters
+max_dist = 5
+
 def main():
 
     rospy.init_node('stanley', anonymous=True)
-    subscriber = Subscriber()
+    sub = Subscriber()
     
     # Start publisher
-    stanley_pub = rospy.Publisher('/stanley_cmd', Float32MultiArray, queue_size=2)
-    stanley_cmd = Float32MultiArray()
+    pub = rospy.Publisher('/cmd_stanley', Float32MultiArray, queue_size=2)
     rate = rospy.Rate(100) # tune based on performance
 
-    while not rospy.is_shutdown():
+    cmd = Float32MultiArray()
+    stanley = StanleyController(sub.pathx, sub.pathy, sub.pathyaw)
 
-        stanley_cmd.data = [v_next, steer_next]
-        stanley_pub.publish(stanley_cmd)
+    while not rospy.is_shutdown():
+        
+        [steer_next, vel_next] = stanley.curvature(sub.xpos, sub.ypos, sub.yaw, sub.vel)
+
+        if np.hypot(sub.xgoal-sub.xpos, sub.ygoal-sub.ypos) > max_dist:
+            cmd.data = [steer_next, vel_next]
+        else:
+            cmd.data = [0,0] # Not sure what to set steering angle to, maybe just leave it in final position?
+        pub.publish(cmd)
 
 if __name__ == '__main__':
     main()
--- ros/docker/stanley/src/subscriber.py
+++ ros/docker/stanley/src/subscriber.py
@@ -1,40 +1,67 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Float32MultiArray
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry, Path
 
 import math
 import numpy as np
 
+# Arbitrary number of waypoints
+n_points = 10
+
+def calculate_yaw(pathx, pathy, start_yaw):
+	yaw = [start_yaw]
+	for i in range(1, len(pathx)):
+		dx = pathx[i] - pathx[i-1]
+		dy = pathy[i] - pathy[i-1]
+		angle = np.arctan2(dy, dx) # calculate the angle between the two points
+		yaw.append(angle)
+	return yaw
+
 # Subscriber Class that updates odometry data and waypoints as the local path changes
 class Subscriber:
     def __init__(self):    
-        self.odom = rospy.Subscriber('/encoder_odom', Odometry, self.car_update)
+        # Subscribed topics
+        # Swap with fused odom topic from ekf in testing
+        self.odom = rospy.Subscriber('/encoder_odom', Odometry, self.update_car)
         self.path = rospy.Subscriber('/path', Path, self.set_waypoints)
-        self.car = Car()
+        self.goal = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.update_goal)
+
+        # Car Info
+        self.xpos = 0
+        self.ypos = 0
+        self.yaw = 0
+        self.vel = 0
+
+        # Path Info
+        self.pathx = np.zeros(n_points)  
+        self.pathy = np.zeros(n_points)
+        self.pathyaw = np.zeros(n_points)
+
+        # Goal Info
         self.xgoal = 0
         self.ygoal = 0
-    
-    def car_update(self,msg):
-        self.car.prev_xpos = self.car.xpos
-        self.car.xpos = msg.pose.pose.position.x
-        self.car.ypos = msg.pose.pose.position.y
-        self.car.yaw = msg.pose.pose.orientation.z
-        self.car.vel = msg.twist.twist.linear.x
-        
-    def set_waypoints(self,msg):
-        waypoints[:,0] = msg.PoseStamped[:].pose.position.x
-        waypoints[:,1] = msg.PoseStamped[:].pose.position.y
-        self.xgoal = msg.PoseStamped[N].pose.position.x
-        self.ygoal = msg.PoseStamped[N].pose.position.y
+        self.yawgoal = 0
 
-# Car class uses Odometry data to update the current position, yaw, and velocity of the car
-class Car():
-    def __init__(self, xpos=0, prev_xpos=0, ypos=0, yaw=0, vel=0):
-        self.xpos = xpos
-        self.prev_xpos = prev_xpos
-        self.ypos = ypos
-        self.yaw = yaw
-        self.vel = vel
+    # Decode Odometry topic to get current position, velocity, and yaw
+    def update_car(self,msg):
+        self.xpos = msg.pose.pose.position.x
+        self.ypos = msg.pose.pose.position.y
+        self.yaw = msg.pose.pose.orientation.z
+        self.vel = msg.twist.twist.linear.x
+        
+    # Decode Path topic into a series of waypoints
+    # The nearest waypoint will be chosen by the Stanley controller
+    def set_waypoints(self,msg):
+        for i in range(0,n_points-1):
+            self.pathx[i] = msg.PoseStamped[i].pose.position.x    # Sets x coordinate of waypoint 
+            self.pathy[i] = msg.PoseStamped[i].pose.position.y    # Sets y coordinate of waypoint 
+        self.pathyaw = calculate_yaw(self.pathx,self.pathy,self.yaw)
+
+    # Decode Goal topic into final x, y, and yaw
+    # Not sure how to properly use this
+    def update_goal(self,msg):
+        self.xgoal = msg.pose.position.x
+        self.ygoal = msg.pose.position.y
+        self.yawgoal = msg.pose.orientation.z
