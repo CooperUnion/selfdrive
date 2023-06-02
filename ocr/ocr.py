785c927595ed1bd5a4b7ad360e1cc1a39c127bd2
Giuseppe Quaratino <giuseppe.quaratino@cooper.edu>
2023-06-02 17:55:26 -0400

    refactor code, args added

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -3,79 +3,89 @@ import pytesseract
 import cv2
 from pytesseract import Output
 import numpy as np
+import argparse
+import copy
+import os
 
 import utils as utils
 
 pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
 config = r'--psm 11 --oem 3 --tessdata-dir /app/ocr/data/tessdata'
 
-src_fpath = '/app/ocr/data/IMG_9185.jpg'
-target_fpath = '/app/ocr/data/img.png'
+class OCR(object):
+    def __init__(self, args):
+        self.debug = args.debug
+        self.conf = args.conf
+        self.src_fpath = args.src_fpath
+        self.target_fpath = '/app/ocr/data/preprocessed.png'
+        self.annotated_img = None
+    
+    def preprocess(self):
+        # Convert the image to png
+        utils.to_png(self.src_fpath, '/app/ocr/data/temp.png')
+        true_img = cv2.imread('/app/ocr/data/temp.png')
+        self.annotated_img = copy.deepcopy(true_img)
 
-# Convert the image to png
-utils.to_png(src_fpath, target_fpath)
+        # Get hsv, sat, and grayscale image
+        hsv_img, sat_img, gray_img = utils.hsv_filter(true_img)
+        if self.debug:
+            cv2.imwrite('/app/ocr/data/hsv.png', hsv_img)
+            cv2.imwrite('/app/ocr/data/sat.png', sat_img)
+            cv2.imwrite('/app/ocr/data/grayhsv.png', gray_img)
 
-# Read image
-true_img = cv2.imread(target_fpath) # np.ndarray
+        # Make the binary image from sat image (0 or 255)
+        _,bin_img = cv2.threshold(sat_img,127,255,cv2.THRESH_BINARY)
+        if self.debug:
+            cv2.imwrite('/app/ocr/data/bin_img.png',bin_img)
 
-# #increase contrast
-# img = utils.inc_cont(img)
-# cv2.imwrite('/app/ocr/data/contrastup.png', img)
-
-# Get hsv, sat, and grayscale image
-hsv_img, sat_img, gray_img = utils.hsv_filter(true_img)
-cv2.imwrite('/app/ocr/data/hsv.png', hsv_img)
-cv2.imwrite('/app/ocr/data/sat.png', sat_img)
-#cv2.imwrite('/app/ocr/data/grayhsv.png', gray_img)
-
-
-#Get octagon mask
-gauss_blur_img = utils.gauss_blur(sat_img)
-_, gauss_blur_binary_img =cv2.threshold(gauss_blur_img, 90, 255, cv2.THRESH_BINARY)
-cv2.imwrite('/app/ocr/data/blur.png', gauss_blur_binary_img)
-
-oct_mask = utils.get_oct(true_img, gauss_blur_binary_img)
-cv2.imwrite('/app/ocr/data/oct.png', oct_mask)
-
-#mask out only octagon
-
-oct_mask = oct_mask[:,:,0]
-#cv2.imwrite('/app/ocr/data/oct.png', oct_mask)
-oct_sat = cv2.bitwise_and(sat_img, oct_mask)
-cv2.imwrite('/app/ocr/data/octsat.png', oct_sat)
-
-# #get red mask 
-# red_mask = utils.get_red(hsv_img)
-# cv2.imwrite('app/ocr/data/redmask.png',red_mask)
+        # Get octagon mask from sat image
+        oct_mask = utils.get_oct(sat_img, self.debug)
+        if self.debug:
+            cv2.imwrite('/app/ocr/data/oct.png', oct_mask)
 
 
-# img = cv2.bitwise_and(img, mask, img)
-# cv2.imwrite('/app/ocr/data/out_mask.png', img)
+        # Make 0 and 1 mask
+        add_mask = np.array(np.logical_not(oct_mask), dtype=np.int8) * 255
+        ocr_img = add_mask + bin_img
+        if self.debug:
+            cv2.imwrite('/app/ocr/data/img2.png',add_mask)
+            cv2.imwrite('/app/ocr/data/img3.png', ocr_img)
+        
 
-# img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-# cv2.imwrite(target_fpath, img)
+        # Apply dilation
+        dilated =utils.dilate(ocr_img)
 
-# # Annotate boxescv2.imwrite('/app/ocr/data/hsv2.png', hsv_img)
-_,octsatbin = cv2.threshold(oct_sat,127,255,cv2.THRESH_BINARY)
-cv2.imwrite('/app/ocr/data/octsatbin.png',octsatbin)
+        # Save the preprocessed image
+        cv2.imwrite(self.target_fpath, dilated)
 
-#make 0 and 1 mask
-add_mask = np.array(np.logical_not(oct_mask), dtype=np.int8) * 255
-ocr_img = add_mask + octsatbin
-cv2.imwrite('/app/ocr/data/img2.png',add_mask)
-cv2.imwrite('/app/ocr/data/img3.png', ocr_img)
 
-erode =utils.erosion(ocr_img)
-cv2.imwrite('/app/ocr/data/erode.png', erode)
+    def run(self):
+        # Run ocr
+        data = pytesseract.image_to_data(Image.open(self.target_fpath), output_type=Output.DICT, config=config)
+        amount_boxes = len(data['text'])
+        for i in range(amount_boxes):
+            if float(data['conf'][i]) > self.conf:
+                print(f'Text recognized: {data["text"][i]} with conf {data["conf"][i]}')
+                (x, y, width, height) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+                self.annotated_img = cv2.rectangle(self.annotated_img, (x, y), (x + width, y + height), (0, 255, 0), 10)
+                self.annotated_img = cv2.putText(self.annotated_img, data['text'][i], (x, y + height + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
-target_fpath = '/app/ocr/data/erode.png'
-data = pytesseract.image_to_data(Image.open(target_fpath), output_type=Output.DICT, config=config)
-amount_boxes = len(data['text'])
-for i in range(amount_boxes):
-    if float(data['conf'][i]) > 10:
-        print(f'Text recognized: {data["text"][i]} with conf {data["conf"][i]}')
-        (x, y, width, height) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
-        sat_img = cv2.rectangle(sat_img, (x, y), (x + width, y + height), (0, 255, 0), 10)
-        sat_img = cv2.putText(sat_img, data['text'][i], (x, y + height + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        
+        cv2.imwrite('/app/ocr/data/out_annotated.png', self.annotated_img)
+    
+    def clean(self):
+        os.system(f'rm {os.getcwd()}/ocr/data/temp.png')
 
-cv2.imwrite('/app/ocr/data/out_annotated.png', sat_img)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='OCR for CUber',
+                                     description='Reads STOP sign.')
+    
+    parser.add_argument('--debug', action='store_true', help='When set to True, saves the images that are created during the preprocessing phase.')
+    parser.add_argument('--src_fpath', type=str, default='/app/ocr/data/IMG_9185.jpg', help='Specify the file path to the input image. Defaults to \'/app/ocr/data/IMG_9185.jpg\'.')
+    parser.add_argument('--conf', type=int, default=60, help='Confidence level threshold. An integer value between 0 and 100. Defaults to 60.')
+    args = parser.parse_args()
+
+    ocr = OCR(args)
+    ocr.preprocess()
+    ocr.run()
+    ocr.clean()
--- ocr/utils.py
+++ ocr/utils.py
@@ -26,16 +26,6 @@ def inc_cont(img:np.ndarray):
 
     return enhanced_img
 
-def gauss_blur(img:np.ndarray):
-    gauss = cv2.GaussianBlur(img, (7,7), 0)
-
-    return gauss
-
-def gray_binary(img:np.ndarray):
-    ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-    gaussthresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-    return gaussthresh
-
 
 def hsv_filter(img: np.ndarray):
     """
@@ -48,77 +38,62 @@ def hsv_filter(img: np.ndarray):
     return hsv_img, s, v
 
 
-def get_oct(img:np.ndarray, gray:np.ndarray):
+def get_oct(img:np.ndarray, debug=False) -> np.ndarray:
     """
-    Takes in an original image (in BGR scale) and a greyscale image.
-    The greyscale image is used for the octagon detection.
-    Returns an original image with an octagon drawn on it.
+    input:
+        saturation channel of hsv image.
+    return:
+        a binary mask of the largest octagon in the image.
+        Inside of the octagon is (255, 255, 255) and the 
+        outside is (0, 0, 0).
     """
-    mask = np.zeros(img.shape)
+    # Add Gaussian Blur to the image
+    gauss = cv2.GaussianBlur(img, (7,7), 0)
 
-    all_oct_img = copy.deepcopy(img)
+    # Make a binary image
+    _, gauss_blur_binary_img =cv2.threshold(gauss, 90, 255, cv2.THRESH_BINARY)
 
-    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #ret, thresh = cv2.threshold(gray, 50, 255, 0)
-    contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Find the contours
+    contours, _ = cv2.findContours(gauss_blur_binary_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     print("Num contours detected:", len(contours))
+
+    if debug:
+        all_oct_img = copy.deepcopy(img)
     
+    # Prepare lists to get the largest octagon
     length_list = []
     approx_list = []
+
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
-        
         if len(approx) == 8:
-            (x, y)=cnt[0, 0]
-
             # Compute the summed length of the edges
             points = np.vstack((cnt, np.expand_dims(cnt[0], 0)))
             length = np.sum(np.linalg.norm(points[1:] - points[:-1], axis=2))
             length_list.append(length)
             approx_list.append(approx)
 
-            cv2.drawContours(all_oct_img, [approx], -1, (0, 255, 255), 3)
+            if debug:
+                cv2.drawContours(all_oct_img, [approx], -1, (0, 255, 255), 3)
     
-    cv2.imwrite('/app/ocr/data/all_oct.png', all_oct_img)
+    if debug:
+        cv2.imwrite('/app/ocr/data/all_oct.png', all_oct_img)
 
     # Find the contour with longest sum of length of edges
     idx = np.where(length_list == max(length_list))[0][0]
     the_octagon = approx_list[idx].reshape(-1, 2)
-    print(f'Best contour:\n{the_octagon}')
+    if debug:
+        print(f'Best contour:\n{the_octagon}')
     
+    # Create a binary mask
+    mask = np.zeros(img.shape)
     cv2.fillPoly(mask, np.int32([the_octagon]), color = (255, 255, 255))
     mask = np.array(mask, dtype=np.uint8)
 
     return mask
 
-def get_red(img:np.ndarray):
-    """
-    Use HSV image to get red mask
-    """
 
-    result= img.copy()
-    #cv2.imwrite("/app/ocr/data/hsv2.png", result)
-    lower = np.array([155,25,0])
-    upper = np.array([179,255,255])
-    #gauss = cv2.GaussianBlur(img, (7,7), 0)
-    mask = cv2.inRange(img, lower, upper)
-    cv2.imwrite("/app/ocr/data/hsv3.png", mask)
-    #result = cv2.bitwise_and(result, result, mask=mask)
-    return mask
-
-    # # Apply Otsu's thresholding to the value channel
-    # _, thres_img = cv2.threshold(v, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    # # Find the red region in the hue channel
-    # lower = np.array([155,25,0])
-    # upper = np.array([179,255,255])
-    # red_mask = cv2.inRange(hsv_img, lower, upper)
-    # cv2.imwrite('/app/ocr/data/redmask.png', red_mask)
-
-    # # Apply the red mask to the thresholded image
-    # masked_img = cv2.bitwise_and(thres_img, thres_img, mask=red_mask)
-
-def erosion(img):
+def dilate(img):
     src = img
     kernel = np.ones((3,3))
     # You can try more different parameters
