32fd4fe6322302ef68f3b7fd120bc4a872822d08
Giuseppe Quaratino <giuseppe.quaratino@tech-slayyy.host.autonomylab.nyc>
2023-06-04 16:36:56 -0400

    Add test scripts

---
+++ ros/docker/techbus/src/parallel_park.py
@@ -0,0 +1,78 @@
+from cand.client import Bus
+import time
+import datetime
+
+import rospy
+from std_msgs.msg import Int16, Float32, Bool
+import math
+
+class LaneOffsetSubscriber:
+    def __init__(self):
+        self.offset = 0
+        self.dist = 0.0
+        self.stop = False
+        rospy.Subscriber('/lane_offset', Int16, self.callback)
+        rospy.Subscriber('/barrel_bool', Bool, self.stop_cb)
+        rospy.Subscriber('/barrel_dist', Float32, self.dist_cb)
+    def callback(self, msg):
+        self.offset = msg.data
+    def stop_cb(self, msg):
+        self.stop = msg.data
+        rospy.loginfo(self.stop)
+    def dist_cb(self, msg):
+        self.dist = msg.data
+        rospy.loginfo(self.dist)
+
+def main():
+    rospy.init_node('lanefollow', anonymous=True)
+    bus = Bus(redis_host='redis')
+    lane_offset = LaneOffsetSubscriber()
+
+    phase = 0
+    phase_start_time = datetime.datetime.now()
+    angle_times = [
+        (25, 2),
+        (16, 1),
+        (4.7, 1),
+        (-7, 1),
+        (-16, 1),
+        (-25, 2)
+    ]
+
+    while True:
+        if phase >= len(angle_times):
+            break
+
+        angle, sec = angle_times[phase]
+        elapsed = datetime.datetime.now() - phase_start_time
+        print(f"phase {phase}: {elapsed}")
+        if datetime.datetime.now() - phase_start_time > datetime.timedelta(seconds=sec):
+            phase_start_time = datetime.datetime.now()
+            phase = phase + 1
+            continue
+        bus.send('DBW_SteeringCommand', {'DBW_steeringAngle': math.radians(angle)})
+        bus.send('DBW_VelocityCommand', {'DBW_linearVelocity': -1.5})
+        time.sleep(0.005)
+
+    start = datetime.datetime.now()
+    while not rospy.is_shutdown():
+        bus.send('DBW_SteeringCommand', {'DBW_steeringAngle': math.radians(angle)})
+        if (datetime.datetime.now() - start) > datetime.timedelta(seconds=0.01):
+            bus.send('DBW_VelocityCommand', {'DBW_linearVelocity': 0})
+        else:
+            bus.send('DBW_VelocityCommand', {'DBW_linearVelocity': -1.5})
+        time.sleep(0.005)
+    
+    # while True:
+    #     if(lane_offset.stop == True):
+    #         bus.send('DBW_VelocityCommand', {'DBW_linearVelocity': 0})
+    #         bus.send('DBW_SteeringCommand', {'DBW_steeringAngle': 0})
+    #         time.sleep(0.005)'
+
+    # # while True:
+    # #     bus.send('DBW_VelocityCommand', {'DBW_linearVelocity': 0})
+    # #     bus.send('DBW_SteeringCommand', {'DBW_steeringAngle': 0})
+    # #     time.sleep(0.005)
+
+if __name__ == "__main__":
+    main()
