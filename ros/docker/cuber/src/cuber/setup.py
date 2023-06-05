eea476cf7f8ad28bced04d4ca2954e21a90c2926
Giuseppe Quaratino <giuseppe.quaratino@cooper.edu>
2023-06-05 00:11:57 -0400

    fix: rename rviz to cuber, add package installation to Dockerfile

---
+++ ros/docker/cuber/src/cuber/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cuber',
+    version='1.0.0',
+    description='Python package for subscribing to and publishing ROS messages',
+    author='Youngwoong Cho',
+    author_email='herocho1997@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
