eaa91be4b83ec485765d1d7cc7112d9a978a1819
Jacob Koziej <jacobkoziej@gmail.com>
2023-10-24 00:29:48 -0400

    Install `rust` with `rust-overlay`

--- flake.nix
+++ flake.nix
@@ -4,22 +4,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
+          overlays = [ (import rust-overlay) ];
         };
+
+        python = pkgs.python311;
+
+        rust-version = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rust = rust-version.override { };
 
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            nixpkgs-fmt
-            python311
-            scons
+          packages = [
+            pkgs.nixpkgs-fmt
+            pkgs.scons
+            rust
+            python
           ];
         };
       }
