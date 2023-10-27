f79ceef9186a5fc9f586df01dd8dbae847f9e02f
Jacob Koziej <jacobkoziej@gmail.com>
2023-10-26 20:59:15 -0400

    Generate `cargo install` commands for crates

---
+++ site_scons/cargo.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import argparse
+import pathlib
+import shlex
+import tomllib
+
+
+def generate_cargo_commands(crates: dict[str, dict[str, str]]) -> list[str]:
+    cmds = []
+
+    for crate, options in crates.items():
+        cmd = ['cargo', 'install']
+
+        flags = [
+            ('git', True),
+            ('branch', True),
+            ('tag', True),
+            ('rev', True),
+            ('locked', False),
+        ]
+
+        for flag, arg in flags:
+            if option := options.get(flag):
+                cmd += [f'--{flag}']
+
+                if arg:
+                    cmd += [shlex.quote(option)]
+
+        cmd += [shlex.quote(crate)]
+
+        cmds += [' '.join(cmd)]
+
+    return cmds
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='cargo.py',
+        description='generate cargo commands',
+    )
+
+    parser.add_argument(
+        '-c',
+        '--config',
+        required=True,
+        type=pathlib.Path,
+    )
+
+    args = parser.parse_args()
+
+    with open(args.config, 'rb') as f:
+        config = tomllib.load(f)
+
+    cmds = generate_cargo_commands(config)
+
+    print('\n'.join(cmds))
