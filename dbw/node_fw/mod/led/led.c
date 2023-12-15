70b32613ce4759bb89ed1925786ed08fa1f0a7cd
ajaku <andyjaku13@gmail.com>
2023-12-15 12:27:01 -0500

    vip report, need to fix bootloader can message (won't build)

---
+++ dbw/node_fw/mod/led/led.c
@@ -0,0 +1,40 @@
+#include "led.h"
+
+#include <driver/gpio.h>
+
+#include "ember_common.h"
+#include "ember_taskglue.h"
+
+// ######        DEFINES        ###### //
+#define LED_GPIO 18
+
+// ######      PROTOTYPES       ###### //
+
+static void led_init();
+static void led_1Hz();
+
+// ######     PRIVATE DATA      ###### //
+static int LED_STATUS;
+
+// ######    RATE FUNCTIONS     ###### //
+
+ember_rate_funcs_S module_rf = {
+    .call_init = led_init,
+    .call_1Hz = led_1Hz,
+};
+
+static void led_init()
+{
+    gpio_pad_select_gpio(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+}
+
+static void led_1Hz()
+{
+    LED_STATUS = !LED_STATUS
+    gpio_set_level(LED_GPIO, LED_STATUS);
+}
+
+// ######   PRIVATE FUNCTIONS   ###### //
+
+// ######   PUBLIC FUNCTIONS    ###### //
