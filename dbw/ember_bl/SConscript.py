822b82768a2267464a0e717c1892522fcacc76d9
Jacob Koziej <jacobkoziej@gmail.com>
2024-02-01 16:20:10 -0500

    Utilize `Phony` when using PlatformIO

--- dbw/ember_bl/SConscript.py
+++ dbw/ember_bl/SConscript.py
@@ -8,13 +8,10 @@ Import('env')
 
 platformio_ini = env.File('platformio.ini')
 
-ember_bl = env.Command(
-    '.ember_bl',
-    platformio_ini,
+ember_bl = env.Phony(
+    'ember_bl',
     'platformio run --project-dir ' + os.path.dirname(str(platformio_ini)),
 )
-env.AlwaysBuild(ember_bl)
-env.Alias('ember_bl', ember_bl)
 
 
 Return('ember_bl')
--- dbw/node_fw/SConscript.py
+++ dbw/node_fw/SConscript.py
@@ -8,13 +8,10 @@ Import('env')
 
 platformio_ini = env.File('platformio.ini')
 
-node_fw = env.Command(
-    '.node_fw',
-    platformio_ini,
+node_fw = env.Phony(
+    'node_fw',
     'platformio run --project-dir ' + os.path.dirname(str(platformio_ini)),
 )
-env.AlwaysBuild(node_fw)
-env.Alias('node_fw', node_fw)
 
 
 Return('node_fw')
