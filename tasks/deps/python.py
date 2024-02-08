720fd4e8e5b4c55cfc4ba6e8e03d32b7963a1f56
Jacob Koziej <jacobkoziej@gmail.com>
2024-02-08 16:05:28 -0500

    Add `deps.python.pip-compile` task

--- tasks/deps/python.py
+++ tasks/deps/python.py
@@ -5,3 +5,8 @@ from invoke import task
 def install_requirements(c):
     c.run('pip install --requirement requirements.txt')
     c.run('pip install --requirement requirements-local.txt')
+
+
+@task
+def pip_compile(c):
+    c.run('pip-compile --output-file requirements.txt')
