7424a2ae06c67ebd729abf1b55c60473176b2046
Jeannette <jett@circe.com>
2024-02-29 14:01:30 -0500

    led blinking on node board

---
+++ dbw/node_fw/mod/led/led.c
@@ -0,0 +1,37 @@
+#include "led.h"
+#include <driver/gpio.h>
+
+#include "ember_common.h"
+#include "ember_taskglue.h"
+
+// ######        DEFINES        ###### //
+#define LED_GPIO 17
+
+// ######     PRIVATE DATA      ###### //
+static int LED_STATUS;
+
+// ######      PROTOTYPES       ###### //
+static void led_init();
+static void led_1Hz();
+
+// ######    RATE FUNCTIONS     ###### //
+
+ember_rate_funcs_S module_rf = {
+    .call_init  = led_init,
+    .call_1Hz = led_1Hz,
+};
+
+static void led_init(){
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+}
+
+static void led_1Hz(){
+    LED_STATUS = !LED_STATUS;
+    gpio_set_level(LED_GPIO, LED_STATUS);
+}
+
+// ######   PRIVATE FUNCTIONS   ###### //
+
+// ######   PUBLIC FUNCTIONS    ###### //
+
+// ######        CAN TX         ###### //
