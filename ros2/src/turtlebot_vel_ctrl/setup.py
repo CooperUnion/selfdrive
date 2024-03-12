3c1777a525f80b68564f2b189ccc53c146113e4a
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2024-03-12 19:18:40 -0400

    lane follow POGGERS

---
+++ ros2/src/turtlebot_vel_ctrl/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtlebot_vel_ctrl'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'vel_ctrl = turtlebot_vel_ctrl.vel_ctrl:main'
+        ],
+    },
+)
