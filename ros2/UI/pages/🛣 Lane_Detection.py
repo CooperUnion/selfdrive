49a334f9259707788c45298813c304733c354b08
Vaibhav.hariani@gmail.com <vaibhav.hariani@cooper.edu>
2024-04-10 18:44:43 -0400

    Added Publisher, specified goals in Homepage.py

--- ros2/UI/pages/🛣 Lane_Detection.py
+++ ros2/UI/pages/🛣 Lane_Detection.py
@@ -6,10 +6,10 @@ from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 from cv_bridge import CvBridge
 import time
+from std_msgs.msg import String
 
-
+## This class currently subscribes to some topics in cam_publisher, which I used to test. Please rewrite for lane Detection.
 class Image_Handler(Node):
-
     def __init__(self):
         self._tabs = None
         self._img = None
@@ -27,27 +27,41 @@ class Image_Handler(Node):
             self.hsvData_callback,
             qos_profile_sensor_data)
 
-
-    def tabs(self,tabs):
+    def tabs(self, tabs):
         self._tabs = tabs
         # SRC is cv2.VideoCapture FOR NOW: Will need to be tweaked in the future to rossify
         self._frame_containers = [tab.empty() for tab in self._tabs]
-
 
     def camData_callback(self, msg_in):
         raw = msg_in
         img = self._bridge.imgmsg_to_cv2(raw)
         self._frame_containers[0].image(img)
-     
+
     def hsvData_callback(self, msg_in):
         raw = msg_in
         img = self._bridge.imgmsg_to_cv2(raw)
         self._frame_containers[1].image(img)
 
-     
+#This class is your publisher: Flesh it out and integrate accordingly
+class Publisher(Node):
+    def __init__(self):
+            super().__init__('Streamlit_Button_Publisher')
+            self.logger = self.get_logger()
+            self.button_pub = self.create_publisher(String, '/streamlit/button',10)
+
+def demo_publish():
+    msg = String()
+    msg.data = "Streamlit Button!"
+    st.session_state["pub_node"].button_pub.publish(msg)
+    st.success("Message Published!")
+
+
+
+
 # See ../../ros2/src/lanefollowingtest/LaneDetection.py
-# This page should render ALL images in there, as well as publish
+# This page should render ALL images in there, as well as publish important update data
 if __name__ == "__main__":
+    #How we Configure the page
     st.set_page_config(
         page_title="Lane Detection",
         page_icon="🛣")
@@ -58,20 +72,30 @@ if __name__ == "__main__":
         "This should render all of the images related to Lane Detection, and relevant parameters.")
     tabs = st.tabs(["Original", "HSV", "Sliding Windows"])
 
-    if "node" not in st.session_state:
-            try:
-                rclpy.init()
-            except RuntimeError:
-                 st.warning("something went wrong performance may be degraded. Try restarting fully")
-            finally:                 
-                handler = Image_Handler()
-                handler.tabs(tabs)
-                st.session_state["node"] = handler
+
+    #This hunk initializes the ROS2 nodes without breaking anything :)
+    if "sub_node" not in st.session_state:
+        try:
+            rclpy.init()
+        except RuntimeError:
+            st.warning(
+                "something went wrong performance may be degraded. Try restarting fully.")
+        finally:
+            handler = Image_Handler()
+            handler.tabs(tabs)
+            st.session_state["sub_node"] = handler
+    if "pub_node" not in st.session_state:            
+            st.session_state["pub_node"] = Publisher()
+
+
+    st.button("Ros Topic Publisher Demo!",on_click=demo_publish)
+   # As this is an infinite loop, it should only be run at the end of the file.
     if render:
-            while(True):
-                try:
-                    rclpy.spin_once(st.session_state['node'])
-                    time.sleep(0.01)                     
-                except:
-                     st.warning("Something went wrong, perhaps tabs were clicked too quickly? Try restarting.")
-                     break
+        while (True):
+            try:
+                rclpy.spin_once(st.session_state['sub_node'])
+                time.sleep(0.01)
+            except:
+                st.warning(
+                    "Something went wrong, perhaps tabs were clicked too quickly? Try restarting.")
+                break
