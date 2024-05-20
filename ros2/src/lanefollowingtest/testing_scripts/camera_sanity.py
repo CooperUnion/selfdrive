a055628a52b0045fa4badedeea0280f63958edc5
Giuseppe Quaratino <giuseppe.quaratino@cooper.edu>
2024-05-20 19:45:11 -0400

    confirmed crosstrack works in car, very precise

--- ros2/src/lanefollowingtest/testing_scripts/camera_sanity.py
+++ ros2/src/lanefollowingtest/testing_scripts/camera_sanity.py
@@ -1,24 +1,16 @@
 import cv2
-import time
-
 
 vidcap_right = cv2.VideoCapture("/dev/video1")
 
 vidcap_left = cv2.VideoCapture("/dev/video3")
 
 while True:
-        # time.sleep(.007)
-        right = vidcap_right.read()
-        if(right[0]):
-            cv2.imshow("right",right[1])
-        # time.sleep(.007)
+    # time.sleep(.007)
+    right = vidcap_right.read()
+    if right[0]:
+        cv2.imshow("right", right[1])
+    # time.sleep(.007)
 
-    
-        left = vidcap_left.read()
-        if(left[0]):
-            cv2.imshow("left", left[1])
-
-
-
-        if cv2.waitKey(10) == 27:
-            break
+    left = vidcap_left.read()
+    if left[0]:
+        cv2.imshow("left", left[1])
