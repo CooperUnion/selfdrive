227bb789005a5b291a6e2716e541a50b4ff0ab35
Giuseppe Quaratino <giuseppe.quaratino@cooper.edu>
2024-05-20 15:37:28 -0400

    Add ability to publish path from lane_change nodee to start testing Stanley controller, need to add multithreading

--- ros2/cmd/src/lane_change.py
+++ ros2/cmd/src/lane_change.py
@@ -1,4 +1,3 @@
-
 import rclpy
 from rclpy.node import Node
 
@@ -10,57 +9,66 @@ from nav_msgs.msg import Odometry, Path
 from std_msgs.msg import Float32MultiArray
 
 import math
-import time
 import numpy as np
 from collections import namedtuple
 
 
 Point = namedtuple("Point", ["x", "y"])
 
+
 def calculate_yaw(pathx, pathy, start_yaw):
-	yaw = [start_yaw]
-	for i in range(1, len(pathx)):
-		dx = pathx[i] - pathx[i-1]
-		dy = pathy[i] - pathy[i-1]
-		angle = np.arctan2(dy, dx) # calculate the angle between the two points
-		yaw.append(angle)
-	return yaw
+    yaw = [start_yaw]
+    for i in range(1, len(pathx)):
+        dx = pathx[i] - pathx[i - 1]
+        dy = pathy[i] - pathy[i - 1]
+        angle = np.arctan2(
+            dy, dx
+        )  # calculate the angle between the two points
+        yaw.append(angle)
+    return yaw
 
 
-class LaneChange(Node): 
+class LaneChange(Node):
     def __init__(self):
         super().__init__('lane_change_node')
 
         self.odom_subscription = self.create_subscription(
             Odometry,
-            '/encoder_odom',    # Temporary until we can replace with filtered odometry message
+            '/encoder_odom',  # Temporary until we can replace with filtered odometry message
             self.odom_callback,
-            10
+            10,
         )
 
         self.path_publisher = self.create_publisher(
-            Path,
-            '/lane_change_path',
-            10)
-        
+            Path, '/lane_change_path', 10
+        )
+
         self.cmd_publisher = self.create_publisher(
-            Float32MultiArray,
-            '/cmd_stanley'
+            Float32MultiArray, '/cmd_stanley', 10
+        )
+
+        timer_period = 0.01  # seconds
+        self.lane_timer = self.create_timer(
+            timer_period, self.follow_path_callback
+        )
+
+        timer_period = 1
+        self.path_timer = self.create_timer(
+            timer_period, self.publish_lane_callback
         )
 
         # Car Info
         self.xpos = 0.0
-        self.ypos = 0.0 
-        self.yaw = 0.0                
+        self.ypos = 0.0
+        self.yaw = 0.0
         self.vel = 0.0
 
         # Path Info
-        self.n_points = 99 # Arbitrary number of waypoints
-        self.max_dist = 1 # max distance from goal point set in meters
-        self.pathx = np.zeros(self.n_points)  
+        self.n_points = 99  # Arbitrary number of waypoints
+        self.max_dist = 1  # max distance from goal point set in meters
+        self.pathx = np.zeros(self.n_points)
         self.pathy = np.zeros(self.n_points)
         self.pathyaw = np.zeros(self.n_points)
-
 
     def odom_callback(self, msg):
         self.xpos = msg.pose.pose.position.x
@@ -70,10 +78,9 @@ class LaneChange(Node):
 
         self.get_logger().info('Received: "%s"' % self.msg)
 
-
     def create_path(self, relative_x, relative_y, end_yaw):
-        wheelbase = 1.27 # double check measurement 
-        lead_dist = math.copysign(wheelbase/2, self.vel)
+        # wheelbase = 1.27  # double check measurement
+        # lead_dist = math.copysign(wheelbase / 2, self.vel)
 
         start_x = self.xpos
         start_y = self.ypos
@@ -87,37 +94,58 @@ class LaneChange(Node):
             end_yaw = coterminal_angle(end_yaw + math.pi)
 
         self.pathx, self.pathy = generate_clothoid_path(
-             Point(start_x, start_y), start_yaw,
-             Point(end_x, end_y), end_yaw    
+            Point(start_x, start_y),
+            start_yaw,
+            Point(end_x, end_y),
+            end_yaw,
+            self.n_points,
         )
         self.pathyaw = calculate_yaw(self.pathx, self.pathy, start_yaw)
+
+    def follow_path_callback(self):
+
+        stanley = StanleyController(self.pathx, self.pathy, self.pathyaw)
+        cmd = Float32MultiArray()
+
+        # while np.hypot(self.pathx[-1]-self.xpos, self.pathy[-1]-self.ypos) > self.max_dist:
+        # may want to set target velocity for stanley curvature rather than actual velocity
+        [steer_next, vel_next] = stanley.curvature(
+            self.xpos, self.ypos, self.yaw, self.vel
+        )
+        cmd.data = [steer_next, vel_next]
+        print(f"Steering Command: {steer_next}, Velocity Command: {vel_next}")
+        # self.cmd_publisher.publish(cmd)
+
+        # time.sleep(.05)
+
+        ### Find a way to cancel path when stanley loop is finished
+        # maybe publish path message at the end with all 0's when the path is complete
+
+    def publish_lane_callback(self):
 
         # publish path message
         path_msg = Path()
 
-        path_msg.poses.pose.position.x = self.pathx
-        path_msg.poses.pose.position.y = self.pathy
-        path_msg.poses.pose.orientation.z = self.pathyaw
+        path_msg.header.frame_id = "map"
+
+        for i in range(self.n_points - 1):
+            pose = PoseStamped()
+
+            pose.header.frame_id = "map"
+
+            pose.pose.position.x = self.pathx[i]
+            pose.pose.position.y = self.pathy[i]
+            pose.pose.position.z = 0.0
+
+            pose.pose.orientation.x = 0.0
+            pose.pose.orientation.y = 0.0
+            pose.pose.orientation.z = self.pathyaw[i]
+            pose.pose.orientation.w = 0.0
+
+            path_msg.poses.append(pose)
 
         self.path_publisher.publish(path_msg)
-        self.get_logger().info('Publishing: "%s"' % path_msg)
-
-    
-    def follow_path(self):
-        stanley = StanleyController(self.pathx, self.pathy, self.vel)
-        cmd = Float32MultiArray()
-
-        while np.hypot(self.pathx[-1]-self.xpos, self.pathy[-1]-self.ypos) > self.max_dist:
-            # may want to set target velocity for stanley curvature rather than actual velocity
-            [steer_next, vel_next] = stanley.curvature(self.xpos, self.ypos, self.yaw, self.vel)
-            
-            cmd.data = [steer_next, vel_next]
-            self.cmd_publisher.publish(cmd)
-
-            time.sleep(.05)
-
-        ### Find a way to cancel path when stanley loop is finished
-        # maybe publish path message at the end with all 0's when the path is complete
+        # self.get_logger().info('Publishing: "%s"' % path_msg)
 
 
 def main(args=None):
@@ -125,14 +153,12 @@ def main(args=None):
 
     lane_change = LaneChange()
 
+    relative_x = 5
+    relative_y = 2
+    end_yaw = 0
+    lane_change.create_path(relative_x, relative_y, end_yaw)
+
     rclpy.spin(lane_change)
-
-    delta_x = 5
-    delta_y = 0
-    delta_yaw = 0
-
-    lane_change.create_path(delta_x, delta_y, delta_yaw)
-    lane_change.follow_path()
 
     lane_change.destroy_node()
     rclpy.shutdown()
