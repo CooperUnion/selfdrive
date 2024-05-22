f1f12ca200f8353163c047771bef6c954ebba01b
Vaibhav.hariani@gmail.com <vaibhav.hariani@cooper.edu>
2024-05-21 20:04:45 -0400

    Added in Generics: This streamlines some tooling that is shared amongst all pages in the UI, particularly the sidebar.

--- UI/ui_generics.py
+++ UI/ui_generics.py
@@ -1,2 +1,7 @@
+import streamlit as st
 
-
+def sidebar():
+    with st.sidebar:
+        st.page_link("Homepage.py", label=":derelict_house_building: Homepage", icon=None, help="Opens Homepage", disabled=False, use_container_width=True)
+        st.page_link("pages/Lane_Detection.py", label=":motorway: Lane Detection", icon=None, help="Opens Lane detection", disabled=False, use_container_width=True)
+        st.page_link("pages/Object_Detection.py", label=":octagonal_sign: Object Detection", icon=None, help="Opens Object detection", disabled=False, use_container_width=True)
