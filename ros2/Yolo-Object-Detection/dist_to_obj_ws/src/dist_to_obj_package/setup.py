861ff15c667962e9100aef1c0c8dd42fe69a1977
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2024-05-25 17:11:25 -0400

    Used string and array instead of custom message

--- ros2/Yolo-Object-Detection/dist_to_obj_ws/src/dist_to_obj_package/setup.py
+++ ros2/Yolo-Object-Detection/dist_to_obj_ws/src/dist_to_obj_package/setup.py
@@ -1,25 +1,25 @@
 from setuptools import find_packages, setup
 
 package_name = 'dist_to_obj_package'
-submodules = 'dist_to_obj_package/object_detection_submodule'
+object_detection_submodule = 'dist_to_obj_package/object_detection_submodule'
+ocr_submodule = 'dist_to_obj_package/ocr_submodule'
 
 setup(
     name=package_name,
     version='0.0.0',
     # packages=find_packages(exclude=['test']),
-    packages=[package_name,submodules],
+    packages=[package_name,object_detection_submodule,ocr_submodule],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools', 'ultralytics'],
+    install_requires=['setuptools', 'ultralytics','pytesseract'],
     zip_safe=True,
 
     maintainer='eeadmin',
     maintainer_email='eeadmin@todo.todo',
     description='TODO: Package description',
-    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
