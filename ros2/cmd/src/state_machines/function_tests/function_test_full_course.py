167c5a0f9e0d4a4bd6dff0724b96c6908a4782e4
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2024-06-03 03:01:48 -0400

    Finished editing state machine

--- ros2/cmd/src/state_machines/function_tests/function_test_full_course.py
+++ ros2/cmd/src/state_machines/function_tests/function_test_full_course.py
@@ -17,6 +17,7 @@ class Function_Test_Q5:
         pothole_detected = False
         white_line_data = []
         barrel_data = []
+        pothole_data = []
 
         while white_lines_count<1:
             white_line_data = self.interface.Object_Detection(
@@ -29,7 +30,7 @@ class Function_Test_Q5:
                 self.interface.Run(white_line_data[1])
                 
 
-        self.interface.Run([4.5, 7.6, math.pi / 2])
+        self.interface.Run([4.5, -7.6, math.pi / 2])
         self.interface.car_sm.Return_To_Follow()
 
         while barrel_count < 1:
@@ -46,7 +47,11 @@ class Function_Test_Q5:
 
         self.interface.car_SM.Obj_Avoidance()
         self.interface.Run([3,0,0]) # might need to change this rel x 
-        self.interface.Run([0.254,-3.048,0])
+        self.interface.Run([0.254,-3.048,0])# come back to this 
+
+        self.interface.car_sm.Return_To_Follow()
+
+
 
         while white_lines_count==1:
             white_line_data = self.interface.Object_Detection(
@@ -57,17 +62,17 @@ class Function_Test_Q5:
             if white_line_data[0]:
                 white_lines_count+=1
                 self.interface.car_sm.Stop_Trigger()
-                self.interface.Run(white_line_data[1])
+                self.interface.Run(white_line_data[1]) # input param needs to be changed
 
         self.interface.car_sm.Turn_Trigger()
    
-        self.interface.Run([4.5, 7.6, math.pi / 2])
+        self.interface.Run([4.5, -7.6, math.pi / 2])
         self.interface.car_sm.Return_To_Follow()
 
 
         while white_lines_count==2:
-            self.interface.Run()
-            person_data = self.interface.Object_Detection(3.5, cared_objects=["Person"], check_in_lane=True)
+            self.interface.Run() # checking in lane might present an issue 
+            person_data = self.interface.Object_Detection(3.5, cared_objects=["Stop Line"], check_in_lane=True)
             if(person_data[0]):
                 self.interface.carSM.Obj_Avoidance()
                 multiplier = 1 if self.interface.current_lane() else -1
@@ -77,30 +82,35 @@ class Function_Test_Q5:
         self.interface.car_SM.Return_To_Follow()
         self.interface.Run()
 
+
+
+        while not pothole_detected:
+            pothole_data = self.interface.Object_Detection(distance_threshold, object_lists=["PotHole"],check_in_lane=True)
+            self.interface.Run()
+            if(pothole_data[0]):
+                self.interface.car_SM.Obj_Avoidance()
+                if(self.interface.current_lane):
+                    self.interface.Run([0.254,-3.048,0])
+                else: 
+                    self.interface.Run([0.254,3.048,0])
+                pothole_detected = True
+
+        self.interface.car_sm.Return_To_Follow()
+
         while barrel_count==2:
             barrel_data = self.interface.Object_Detection(0.9, cared_objects=["Barrel"],)
             self.interface.Run()
 
             if(barrel_data[0]):
                 barrel_detected = True
-                self.interface.car_SM.Stop_Trigger()
-                self.interface.Run((barrel_data[1]+0.4) - 1.524)
-        
-        self.interface.car_SM.Return_To_Follow()
-        self.interface.Run()
-
-        while not pothole_detected:
-            barrel_data = self.interface.Object_Detection(distance_threshold, object_lists=["PotHole"],check_in_lane=True)
-            self.interface.Run()
-            if(barrel_data[0]):
                 self.interface.car_SM.Obj_Avoidance()
                 if(self.interface.current_lane):
                     self.interface.Run([0.254,-3.048,0])
                 else: 
                     self.interface.Run([0.254,3.048,0])
-                barrel_detected += 1
-
-        self.interface.car_sm.Return_To_Follow()
+        
+        self.interface.car_SM.Return_To_Follow()
+        self.interface.Run()
 
         while white_lines_count==3:
             white_line_data = self.interface.Object_Detection(
@@ -109,9 +119,9 @@ class Function_Test_Q5:
             self.interface.Run()
 
             if white_line_data[0]:
+                white_lines_count+=1
                 self.interface.car_sm.Stop_Trigger()
                 self.interface.Run(white_line_data[1])
-                white_lines_count+=1
 
 if __name__ == "__main__":
     main("Function_Test_Q3", Function_Test_Q5)
