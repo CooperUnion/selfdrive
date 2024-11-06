9019e67a95d0dac7a75b05339772c7f8b018f745
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-05 23:55:19 -0500

    Add `python3Packages.cantools` to overlay

--- nix/overlays/python.nix
+++ nix/overlays/python.nix
@@ -1,4 +1,14 @@
 { final, prev }:
 python-final: python-prev: {
+  cantools = python-prev.cantools.overridePythonAttrs (_: {
+    version = "37.0.7";
+    src = prev.fetchFromGitHub {
+      owner = "CooperUnion";
+      repo = "cantools-autonomylab";
+      rev = "4eb9008df23becafab0e8e257e61b8d07264dec2";
+      hash = "sha256-JobJCK2C+IhxOjuKmrQgZeUWksXaa/C6xI2FCGGmcMI=";
+    };
+    doCheck = false;
+  });
   opencan-cand = python-final.callPackage ../pkgs/development/python-modules/opencan-cand.nix { };
 }
